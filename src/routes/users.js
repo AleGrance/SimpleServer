5a7441560b46a28412a3e3c7bc7acb827fb1ea78
alegrance <alegrance1000@gmail.com>
2023-02-27 21:57:58 -0300

    upd users route add token security

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,41 +1,81 @@
 var CryptoJS = require("crypto-js");
 module.exports = app => {
     const Users = app.db.models.Users;
+    const htmlBody = `
+  <div style="text-align: center;">
+  <h1>ERROR 403</h1>
+  <br>
+  <img src="http://i.stack.imgur.com/SBv4T.gif" alt="I choose you!"  width="250" />
+  <br>
+  <h1>Forbidden</h1>
+  </div>
+  `;
+    const apiToken = "35dd7b33-3c2a-48a4-827f-042e57c9c3b8";
+
 
     app.route('/users')
         .get((req, res) => {
-            Users.findAll()
-                .then(result => res.json(result))
-                .catch(error => {
-                    res.status(402).json({
-                        msg: error
-                    });
+            if (!req.headers.apitoken) {
+                return res.status(403).send({
+                    error: "Forbidden",
+                    message: "Tu petición no tiene cabecera de autorización",
                 });
-        })
-        .post((req, res) => {
-            // Receiving data
-            const {
-                user_name,
-                user_password,
-                user_email,
-                user_fullname,
-                role_id
-            } = req.body;
-            // Creating new user
-            const user = {
-                user_name: user_name,
-                user_password: user_password,
-                user_email: user_email,
-                user_fullname: user_fullname,
-                role_id: role_id,
             }
-            // Encrypting password
-            user.user_password = CryptoJS.AES.encrypt(user.user_password, 'secret').toString();
-            // Insert new user
-            Users.create(user)
-                .then(result => res.json(result))
-                .catch(error => res.json(error.message));
+
+            if (req.headers.apitoken === apiToken) {
+                Users.findAll()
+                    .then(result => res.json(result))
+                    .catch(error => {
+                        res.status(402).json({
+                            msg: error
+                        });
+                    });
+            } else {
+                return res.status(403).send({
+                    error: "Forbidden",
+                    message: "Cabecera de autorización inválida",
+                });
+            }
         })
+
+        .post((req, res) => {
+            if (!req.headers.apitoken) {
+                return res.status(403).send({
+                    error: "Forbidden",
+                    message: "Tu petición no tiene cabecera de autorización",
+                });
+            }
+
+            if (req.headers.apitoken === apiToken) {
+                // Receiving data
+                const {
+                    user_name,
+                    user_password,
+                    user_email,
+                    user_fullname,
+                    role_id
+                } = req.body;
+                // Creating new user
+                const user = {
+                    user_name: user_name,
+                    user_password: user_password,
+                    user_email: user_email,
+                    user_fullname: user_fullname,
+                    role_id: role_id,
+                }
+                // Encrypting password
+                user.user_password = CryptoJS.AES.encrypt(user.user_password, 'secret').toString();
+                // Insert new user
+                Users.create(user)
+                    .then(result => res.json(result))
+                    .catch(error => res.json(error.message));
+            } else {
+                return res.status(403).send({
+                    error: "Forbidden",
+                    message: "Cabecera de autorización inválida",
+                });
+            }
+        });
 
     app.route('/users/:user_id')
         .get((req, res) => {
