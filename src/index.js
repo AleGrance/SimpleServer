07fd33573d6bb76320b71c444fddaa663e9de219
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2024-04-19 11:53:48 -0400

    add hetmet dotenv apikey on headers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import consign from 'consign';
+const helmet = require("helmet");
+
+// Importar para leer los valores de entorno
+require('dotenv').config()
 
 const app = express();
+
+// Usar modulo de seguridad HELMET
+app.use(helmet());
 
 //cross access
 var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, apikey');
     next();
 }
 app.use(allowCrossDomain);
